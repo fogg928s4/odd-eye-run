94df903c2809d1a701ac63e8a1aa8b495747c4a3
Jose Melgares <rjmzelaya@gmail.com>
2026-03-04 12:42:45 +0100

    fix: adds mode header

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
-#include 'headers/modes.h'
+#include "headers/modes.h"
 
 int main(int argc, char* argv[]) {
     puts("Odd Eye Runner");
